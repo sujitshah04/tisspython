d235efbe2cad192a5fc66f5166ec5dddce4df437
Shraddha-kr <shraddhakr@outlook.com>
2025-02-02 16:39:38 +0530

    updates as on Feb 2

---
+++ basics/basics.md
@@ -0,0 +1,2 @@
+* Python is case sensitive, so it is important to use the correct case while naming variables or objects.
+* Do not use any dedicated words that Python already uses, such as print, input.
---
+++ python-by-example/ch1_basics.py
@@ -0,0 +1,20 @@
+num1 = 93
+num2 = 100
+
+# basic maths operations
+ans_add = num1 + num2
+print("ans_add",  ans_add)
+
+ans_sub = num1 - num2
+print("ans_sub",  ans_sub)
+
+ans_mul = num1 * num2
+print("ans_mul",  ans_mul)
+
+ans_div = num1 // num2
+print("ans_div",  ans_div) # whole number division 
+
+ans_divi = num1 / num2
+print("ans_divi",  ans_divi) # decimal number division
+
+print("First line\n Second line") # \n is used as a line break"
