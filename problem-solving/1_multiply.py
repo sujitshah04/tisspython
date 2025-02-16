8472396c57a08ae8d7798015c462dd66ee420908
sujitshah04 <sujit4594@gmail.com>
2025-02-16 12:36:41 +0530

    Ans_1_Multiplication Function

--- problem-solving/1_multiply.py
+++ problem-solving/1_multiply.py
@@ -1,9 +1,16 @@
 # This mission is the easiest one. Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
 
 # Input: Two arguments. Both are of type int.
+a=int(input('Enter first number: '))
+b=int(input('Enter second number: '))
+
 
 # Output: Int.
+def mult_two(a,b):
+    ans=a*b
+    return ans
 
+print(f"Multiplication of {a} and {b} is: {mult_two(a,b)}")
 # Examples:
 
 # assert mult_two(3, 2) == 6
