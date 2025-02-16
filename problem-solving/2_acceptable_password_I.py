125ed6031e722d9c52ae130ad6fc6588ec80adaf
sujitshah04 <sujit4594@gmail.com>
2025-02-16 12:52:13 +0530

    Ans_2_Acceptable_password

--- problem-solving/2_acceptable_password_I.py
+++ problem-solving/2_acceptable_password_I.py
@@ -2,10 +2,21 @@
 # You are at the beginning of a password series. Every mission is based on the previous one. The missions that follow will become slightly more complex.
 
 # In this mission, you need to create a password verification function.
-
 # The verification condition is:
-
 # the length should be bigger than 6.
+
+int_pass=input('Enter your password string: ')
+
+def acceptable_password(int_pass):
+    if len(int_pass)<=6:
+        result=False
+    else:
+        result=True
+    return result
+
+print(acceptable_password(int_pass))
+
+
 # Input: A string (str).
 
 # Output: A logic value (bool). -->
