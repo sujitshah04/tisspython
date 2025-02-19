72aa28023d0927b89e819b3104cf17d1b1384273
sujitshah04 <sujit4594@gmail.com>
2025-02-19 19:48:55 +0530

    Ans_4 Reverse String

--- problem-solving/4_reverse_string.py
+++ problem-solving/4_reverse_string.py
@@ -1,1 +1,13 @@
 # Reverse a string and print it
+
+str1=input('Enter your string: ')
+
+def revese_str(str1):
+    rev_str1=''
+    
+    for i in range(len(str1)-1,0,-1):
+        rev_str1+=str1[i]        
+        
+    return rev_str1
+    
+print(revese_str(str1))
